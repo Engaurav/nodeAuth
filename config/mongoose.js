c5f938c9abdf55849f86c952392e8fe1e0edcdf3
Gaurav Singh <gaurav.singh323@gmail.com>
2022-06-10 18:40:41 +0530

    HEroku change 3

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
-const nodeAuthCloudDB = `mongodb+srv://gauravsingh323:8OlAFfvtjlWq4gYt@cluster0.guddo.mongodb.net/node_authenticate`;
+const nodeAuthCloudDB =
+  process.env.MONGO_URL ||
+  `mongodb+srv://gauravsingh323:8OlAFfvtjlWq4gYt@cluster0.guddo.mongodb.net/node_authenticate`;
 // const nodeAuthLocalDB = `mongodb://localhost:27017/node_authenticate`;       In Case of Local Database
 // Connectiong mongoose to database
 mongoose.connect(nodeAuthCloudDB);
 
-
-
 const db = mongoose.connection;
 
-db.on('err',console.error.bind(console,'Error in Connection of DB'));
+db.on("err", console.error.bind(console, "Error in Connection of DB"));
 
-db.once('open',function(){
-    console.log('Connected to Database');
+db.once("open", function () {
+  console.log("Connected to Database");
 });
 
 module.exports = db;
