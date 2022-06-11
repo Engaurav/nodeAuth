cd664d250f182119bce05d934a8e881b8ea43241
Gaurav Singh <gaurav.singh323@gmail.com>
2022-06-11 13:34:26 +0530

    Google Passport Login 1

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -7,7 +7,7 @@ const User = require('../model/user');
 passport.use(new googleStrategy({
     clientID : process.env.GOOGLE_CLIENT_ID ,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET ,
-    callbackURL : process.env.GOOGLE_CALLBACK_URL "
+    callbackURL : process.env.GOOGLE_CALLBACK_URL
     },
     function(accessToken,refreshToken,profile,done){
         //find a user
